8bc8ee110d12a55a9109b854c634a2ca4e248fa2
glenbot <glenbot@gmail.com>
2011-09-06 08:52:30 -0500

    Added a better unicode description to the Slot model so you can tell the difference between slots of the same time

--- symposion/schedule/views.py
+++ symposion/schedule/views.py
@@ -252,9 +252,8 @@ def schedule_conference(request):
     ctx = {
         "user_hash": user_hash,
         "timetables": [
-            Timetable(Slot.objects.filter(start__week_day=3), user=request.user),
-            Timetable(Slot.objects.filter(start__week_day=4), user=request.user),
             Timetable(Slot.objects.filter(start__week_day=5), user=request.user),
+            Timetable(Slot.objects.filter(start__week_day=6), user=request.user),
         ],
         "timezone": settings.SCHEDULE_TIMEZONE,
         "csrf_token": csrf(request),
